28dbd8b49d1da9375b1c11208a688fc05df83045
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:11:40 +0000

    Add files via upload

---
+++ 0019-WHILE STATEMENT INFINITE LOOP/0019/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;  /*WHILE STATEMENT INFINITE LOOP*/
+namespace _0019
+{
+    class Program
+    {
+ static void Main(string[] args)
+    {
+    int index = 1;    while (index <=500)
+    {
+     Console.WriteLine(index);
+index++;   /* TAKE AWAY THIS LINE 
+         TO CREATE AN INFINITE LOOP*/
+     }    Console.ReadLine();        }  }  }
