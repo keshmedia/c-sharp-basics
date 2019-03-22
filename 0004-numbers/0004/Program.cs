2b320f1504c0d8e951106ef7aae298240c7e0291
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:51:07 +0000

    Add files via upload

---
+++ 0004-numbers/0004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _0004
+{//numbers//
+   class Program
+   {
+       static void Main(string[] args)
+       {
+   Console.WriteLine( 22 + 45 );
+   Console.WriteLine( 456 - 45 );
+   Console.WriteLine( 16 * 45 );
+   Console.WriteLine( 578 / 45 );
+//more//
+  Console.WriteLine(578 % 45);
+  Console.WriteLine( 5 + 4 * 3 );
+  Console.WriteLine( (5 + 4) * 3);
+  Console.WriteLine( 6.7 + 1.2 );
+            Console.ReadLine();   } }}
