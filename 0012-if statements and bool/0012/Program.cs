b48b0e167a1c5ac17d289a46a7c575bc16d46379
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:59:59 +0000

    Add files via upload

---
+++ 0012-if statements and bool/0012/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // if statements //
+namespace _0012
+{
+    class Program
+    {
+ static void Main(string[] args)
+ {
+   bool isMale = false;
+   bool isTall = false;
+   if (isMale || isTall) // &&  ||   and or // 
+            {
+   Console.WriteLine("you are a tall male");
+   } else if (isMale && !isTall) { // ! not //
+   Console.WriteLine("you are a short male");
+   } else if (!isMale && isTall) {
+  Console.WriteLine("you are not male but you are tall");
+   } else {
+  Console.WriteLine("you are either not male or not tall");
+   }         Console.ReadLine();   }  }}
