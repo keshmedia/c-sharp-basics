9af66860532b7698e9264e8b89ae524509b12816
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:57:58 +0000

    Add files via upload

---
+++ 0010-methods/0010/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;  //methods//
+namespace _0010
+{
+  class Program
+  {
+static void Main(string[] args)
+{
+SayHi("Paddy", 55);
+SayHi("John", 66);
+SayHi("Peter", 354);
+Console.ReadLine();
+}
+static void SayHi( string name, int age )
+{
+Console.WriteLine("hello " + name + " you are " + age);
+  }  } }
