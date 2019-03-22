e9eb566b50a198f3f51642f8e277959ba874bd9b
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:55:00 +0000

    Add files via upload

---
+++ 0007-calculator/0007/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;  //calculator//
+namespace _0007
+{
+ class Program
+ {
+ static void Main(string[] args)
+ {
+Console.Write("enter a number:  ");
+  double num1 =  Convert.ToDouble (Console.ReadLine());
+Console.Write("enter another number:  ");
+  double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(num1 + num2);
+Console.ReadLine();       }  }}
