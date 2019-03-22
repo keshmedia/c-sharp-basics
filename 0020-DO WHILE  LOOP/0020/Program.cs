24e29e8509b46069f6182e4c8523a47a6f99c240
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:12:54 +0000

    Add files via upload

---
+++ 0020-DO WHILE  LOOP/0020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _0020
+{/* DO WHILE  LOOP*/
+    class Program
+    {
+   static void Main(string[] args)
+   {
+       int index = 6;
+      do 
+//RUN INSIDE LOOP ONE TIME ANYWAY//
+    {
+        Console.WriteLine(index); index++;
+    } while (index <= 5) ;    Console.ReadLine();
+  } }}
