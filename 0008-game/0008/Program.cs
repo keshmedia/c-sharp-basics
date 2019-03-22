76ec84a5193ea4e24378a2885c7d61e3f1fa07dd
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:56:02 +0000

    Add files via upload

---
+++ 0008-game/0008/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; //game//
+namespace _0008
+{
+    class Program
+    {
+  static void Main(string[] args)
+      {
+  string colour, pluralNoun, celebrity;
+  Console.Write("enter a colour:  ");
+  colour = Console.ReadLine();
+  Console.Write("enter a plural noun:  ");
+  pluralNoun = Console.ReadLine();
+  Console.Write("enter a celebrity:  ");
+  celebrity = Console.ReadLine();
+  Console.WriteLine("Rose are "+ colour);
+  Console.WriteLine(pluralNoun + " are blue");
+  Console.WriteLine("I Love " + celebrity);
+  Console.ReadLine();  }  }}
