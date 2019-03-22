da022e1fb449f77a398f81d42c8eeda0b0d45ed2
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:48:36 +0000

    Add files via upload

---
+++ 0002- data types/0002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _0002
+{
+    class Program  //DATA TYPES//
+    {
+        static void Main(string[] args)
+        {
+  string pharse = "Kesh Kreations";
+  char myGrade = 'C';
+  int age = 50;
+  double pounds = 3.3;
+  decimal crypto = 45;
+  bool isMale = true;
+
+Console.WriteLine(pharse.Length);
+Console.WriteLine(pharse.IndexOf('s')); 
+Console.ReadLine(); }}}
