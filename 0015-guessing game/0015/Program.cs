945c2b8dcbe7613a1fdbaae97033a6c3a7a67c47
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:03:27 +0000

    Add files via upload

---
+++ 0015-guessing game/0015/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;   //guessing game//
+namespace _0015
+{
+    class Program
+    {
+     static void Main(string[] args)
+    {
+ string secretWord = "cat"; // answer //
+string guess = "";    int guessCount = 0;
+ int guessLimit = 3; //set guess number
+ bool outOfGuesses = false;   while (guess != secretWord && !outOfGuesses)
+ {   if (guessCount < guessLimit)  {
+  Console.Write("enter guess: ");  guess = Console.ReadLine();  guessCount++;
+ }   else { outOfGuesses = true; } }
+ if (outOfGuesses)  {   Console.Write("You Lose !!!!");   }
+ else { Console.Write("You Win !!!!"); }
+ Console.ReadLine();              }  }}
