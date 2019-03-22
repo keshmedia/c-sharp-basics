97eca0868477d5532999bb98739ee91f7eac0f7e
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:53:38 +0000

    Add files via upload

---
+++ 0006-user input/0006/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;   //INPUT//
+namespace _0006
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+   Console.Write("Enter you name:  ");
+   string name = Console.ReadLine();
+   Console.Write("Enter you age:  ");
+   string age = Console.ReadLine();
+Console.WriteLine("Hello " + name + " you are " + age);
+Console.ReadLine();    }  }}
