4312e21515b85cb6831cbab50ff957174383244d
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:46:49 +0000

    Add files via upload

---
+++ 0001-vars/0001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace _0001
+{
+  class Program
+    {
+static void Main(string[] args)
+    {
+// VARIABLES //
+string characterName = "Jim, Paddy ";
+int characterAge = 85;
+Console.WriteLine("There once was a man named " + characterName);
+Console.WriteLine("He was " + characterAge + " years old " );
+Console.WriteLine("He like being "  +  characterName );
+Console.WriteLine("But he did'nt like being "  + characterAge );
+Console.ReadLine();     } } }
