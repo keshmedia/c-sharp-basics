9fdc779492a8ffa8633ee9dd9a9d565263af0ca1
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:14:04 +0000

    Add files via upload

---
+++ 0021-FOR LOOP/0021/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _0021
+ { // FOR LOOP //
+    class Program
+ {
+ static void Main(string[] args)
+ {
+        for(int i =1; i <= 5; i++)
+ {
+     Console.WriteLine(i);
+ }
+     Console.ReadLine();
+ }    }   }
