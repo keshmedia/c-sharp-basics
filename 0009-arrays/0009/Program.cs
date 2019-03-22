a23543630ce63a07b52b3a142a02e9f5b14b5514
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:57:03 +0000

    Add files via upload

---
+++ 0009-arrays/0009/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;   //arrays//
+namespace _0009
+{
+    class Program
+    {
+static void Main(string[] args)
+{
+ int[] myNumbers = { 5, 8, 56, 22, 28, 33};
+ string[] friends = new string[5];
+ friends[0] = "Paddy";
+ friends[4] = "Peter";
+ friends[2] = "John";
+
+Console.WriteLine(myNumbers[0] );
+Console.ReadLine();   }}}
