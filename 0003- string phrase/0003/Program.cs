295102244ce7ca49756b3e3bad48bc195f2c796b
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:49:47 +0000

    Add files via upload

---
+++ 0003- string phrase/0003/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _0003
+{
+    //STRINGS//
+    class Program
+    {
+ static void Main(string[] args)
+ {
+   //\n for new line//
+     string phrase = "Kesh\nKreations" + " is Cool";
+     Console.WriteLine(phrase.Contains("K"));
+     Console.ReadLine();
+ }}}
