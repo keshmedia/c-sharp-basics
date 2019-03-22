a4710349cb26d1f1006dd2543e3546a62566a058
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:18:12 +0000

    Add files via upload

---
+++ Guess Number with TRY CATCH/Guess Number/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+namespace Guess_Number
+{
+    public partial class MainWindow : Window
+    {
+  private int lives = 30;
+  private int random = 0;
+  public MainWindow()
+  {
+      InitializeComponent();
+      Random rnd = new Random();
+      random = rnd.Next() % 100;
+  }
+  private void TxbInput_KeyDown(object sender, KeyEventArgs e)
+  {
+            //TRY//
+      try
+      {
+ if (lives == 0)
+ {
+     lblFrom.Content = "You ";
+     lblTo.Content = "Lose ";
+     return;
+ }
+ if (e.Key == Key.Enter)
+ {
+ lives--;
+
+ int userGuessed = Int32.Parse(txbInput.Text);
+ if (userGuessed == random)
+ {
+     lblFrom.Content = "You ";
+     lblTo.Content = "Win ";
+     return;
+ }
+ if (userGuessed < random)
+ {
+     lblFrom.Content = userGuessed;
+ }
+ else
+ {
+     lblTo.Content = userGuessed;
+ }
+ lblStatus.Content = "Remaining Lives:  " + lives;
+ if (lives <= 3)
+ {
+ }
+  lblStatus.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+      }
+ }
+            //CATCH//
+ catch
+ {
+     MessageBox.Show("YOU MUST ENTER A NUMBER !!!!");
+ }     }   }}
