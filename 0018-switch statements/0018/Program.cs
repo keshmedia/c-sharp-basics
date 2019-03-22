6b209d09d7ab71ba7de9df7871ff1a52d96c1923
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:10:29 +0000

    Add files via upload

---
+++ 0018-switch statements/0018/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;   //switch statements//
+namespace _0018
+{
+class Program
+    {  static void Main(string[] args)   { Console.WriteLine(GetDay(5)); 
+  Console.ReadLine();   }  static string GetDay(int dayNum)  {
+ string dayName; switch (dayNum)
+            {
+    case 0:   dayName = "SUNDAY";                     break;
+    case 1:   dayName = "MONDAY";                     break;
+    case 2:   dayName = "TUESDAY";                    break;
+    case 3:   dayName = "WEDNESDAY";              break;
+    case 4: dayName = "THURDAY";                      break;
+    case 5:   dayName = "FRIDAY";                         break;
+    case 6: dayName = "SATURDAY";                     break;
+ default:  dayName = "INVALID DAY NUMBER";    break; }
+            return dayName;   }  } }
