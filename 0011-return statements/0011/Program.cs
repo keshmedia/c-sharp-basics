c7604a8b60b8ae919f6b467192548fc420df8ce0
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:58:54 +0000

    Add files via upload

---
+++ 0011-return statements/0011/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;    // return statements//
+namespace _0011
+{
+    class Program
+    {
+  static void Main(string[] args)
+  {
+  int cubeNumber = cube(5);
+  Console.WriteLine(cubeNumber);
+  Console.ReadLine();
+  }
+  static int cube(int num)
+  {
+   int result = num * num * num;
+   return result;          } } }
