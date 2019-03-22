aa544f59a47e51ef39285fe43fcebd1e3db17c2b
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:04:33 +0000

    Add files via upload

---
+++ 0016-while loop/0016/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;    //while loop// 
+namespace _0016
+{
+  class Program
+{
+static void Main(string[] args)
+{
+ int index = 1;
+ while (index <= 50)
+{
+Console.WriteLine(index); index++;
+}
+ Console.ReadLine();
+} } }
