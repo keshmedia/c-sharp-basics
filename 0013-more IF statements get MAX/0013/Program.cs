ffccd8988f4cd1d123e5d0937a099ab539fd40f9
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:01:11 +0000

    Add files via upload

---
+++ 0013-more IF statements get MAX/0013/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;  //more IF statements get MAX //
+namespace _0013
+{
+    class Program
+    {
+  static void Main(string[] args)
+  {
+      Console.WriteLine(GetMax(2, 10));   Console.ReadLine();
+  }
+   static int GetMax(int num1, int num2)
+  {
+ int result;         if (num1 > num2)
+ {
+     result = num1;
+ }
+ else {
+     result = num2;
+ }            return result;    }  } }
