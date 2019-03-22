ad9379b3b158f998c1b3ee9c153d9a6d57d15a13
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:02:22 +0000

    Add files via upload

---
+++ 0014-Calculator with try catch ERROR/0014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;   // Calculator with try catch ERROR //
+namespace _0014
+{
+   class Program
+  {
+      static void Main(string[] args)
+   { try
+   {
+  Console.Write("enter a number: "); double num1 = Convert.ToDouble(Console.ReadLine());
+  Console.Write("enter operator: ");   string op = Console.ReadLine();
+  Console.Write("enter a number: ");  double num2 = Convert.ToDouble(Console.ReadLine());
+  if (op == "+") { Console.WriteLine(num1 + num2);  } else if (op == "-")
+  { Console.WriteLine(num1 - num2);  }
+  else if (op == "/") { Console.WriteLine(num1 / num2); }
+  else if (op == "*") { Console.WriteLine(num1 * num2); }
+  else { Console.WriteLine("Invalid Operator"); }
+   }catch {  Console.WriteLine("ERROR !!!!!!!! TRY AGAIN");
+   }   Console.ReadLine();        }  } }
