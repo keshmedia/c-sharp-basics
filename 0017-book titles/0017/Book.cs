b5e18fdd3dca87bf1add901c5d8dddcfdad415f7
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 02:09:39 +0000

    Add files via upload

---
+++ 0017-book titles/0017/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace _0017
+{
+    public class Book
+    {
+  public string title;
+  public string author;
+  public int pages; 
+
+public Book(string aTitle, 
+string aAuthor, int aPages)
+        {
+ title = aTitle;
+ author = aAuthor;
+ pages = aPages;
+
+ } } }
---
+++ 0017-book titles/0017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace _0017
+{
+public class Program
+  {
+      static void Main(string[] args)
+      {
+Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+Book book2 = new Book("Lord of the rings", "Tolkein", 7000);
+
+Console.WriteLine(book1.title);
+Console.WriteLine(book1.author);
+Console.WriteLine(book1.pages); 
+
+Console.WriteLine(book2.title);
+Console.WriteLine(book2.author);
+Console.WriteLine(book2.pages);
+            Console.ReadLine();
+ }  } }
