12ce7762f295ba699b9de5b3dcb4ba44ca560034
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-03-22 01:52:13 +0000

    Add files via upload

---
+++ 0005- more numbers/0005/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; //math operations NUMBERS//
+namespace _0005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  Console.WriteLine( Math.Abs(-55) );
+  Console.WriteLine(Math.Pow( 4.5, 3 ) );
+  Console.WriteLine( Math.Sqrt(454));
+  Console.WriteLine( Math.Max (45, 675) );
+  Console.WriteLine( Math.Min(45, 675));
+  Console.WriteLine( Math.Round(3.7) );
+  Console.WriteLine( Math.PI );
+  Console.ReadLine(  );      }  }}
